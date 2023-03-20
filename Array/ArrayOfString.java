26098bc63253e785815b84b36c5b6287d7318b28
abhii2002 <as0709716@gmail.com>
2023-03-20 20:37:21 +0530

    Create ArrayOfString.java

---
+++ Array/ArrayOfString.java
@@ -0,0 +1,11 @@
+package com.blissvine.Array;
+
+import java.util.Arrays;
+
+public class ArrayOfString {
+    public static void main(String[] args){
+         String[] persons = {"Abhishek", "Milky", "Kajal", "Utkarsh"};
+         System.out.println(Arrays.toString(persons));
+
+    }
+}
