f598f42274b64a912f009107afa5fde25b1a9cfb
abhii2002 <as0709716@gmail.com>
2023-04-24 19:30:59 +0530

    Create BiNumberRunner.java

---
+++ Exercises/BiNumberRunner.java
@@ -0,0 +1,21 @@
+package com.blissvine.Exercises;
+
+public class BiNumberRunner {
+    public static void main(String[] args){
+
+        BiNumber numbers = new BiNumber(2,3);
+
+        System.out.println(numbers.add());
+        System.out.println(numbers.multiply());
+        System.out.println(numbers.getNumber1());
+        System.out.println(numbers.getNumber2());
+
+        numbers.Double();
+        System.out.println(numbers.getNumber1());
+        System.out.println(numbers.getNumber2());
+
+
+    }
+
+
+}
